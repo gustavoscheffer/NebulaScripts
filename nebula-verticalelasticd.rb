1df9a86631f4a09f5970bc32b7e41794110ea370
gustavo <gustavo.scheffer@outlook.com>
2018-08-19 21:37:28 -0300

    new entry

--- nebula-verticalelasticd.rb
+++ nebula-verticalelasticd.rb
@@ -63,7 +63,7 @@ end
 #3) Verificar se estas máquinas ultrapassaram o limite de hardware (memoria ou cpu);
 vms_filtradas.each do |vm_filtrada|
   #puts vm_filtrada.monitoring_xml
-  puts vm_filtrada.monitoring(['MONITORING/CPU'])
+  puts vm_filtrada.monitoring(['MONITORING/CPU', 'MONITORING/MEMORY'])
 end
 
 
