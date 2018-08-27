4105649a7c54be46f3f94b5ac804c14df9f886fe
Gustavo Scheffer <gustavo.scheffer@outlook.com>
2018-08-26 22:02:14 -0300

    20180826

--- nebula-verticalelasticd.rb
+++ nebula-verticalelasticd.rb
@@ -74,14 +74,16 @@ end
 cpu_metrics_by_vm = Hash.new
 cpu_values = Array.new
 
-
 i = 0
 vms_filtradas.each do |vm_filtrada|
   #puts vm_filtrada.monitoring_xml
   cpu_metrics_by_vm = vm_filtrada.monitoring(['MONITORING/CPU'])
   cpu_values = cpu_metrics_by_vm.fetch('MONITORING/CPU')
-  puts cpu_values
 end
+
+puts cpu_values.class
+puts  cpu_values
+
 
 #verifica mais metricas de cada vm
 cpu_metrics_by_vm.each do |metric_by_vm|
