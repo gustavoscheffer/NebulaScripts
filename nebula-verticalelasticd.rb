8966f85b840cf20c1cb9530dc66c8053d6f0b78c
Gustavo Scheffer <gustavo.scheffer@outlook.com>
2018-08-31 12:17:56 -0300

    20180831

--- nebula-verticalelasticd.rb
+++ nebula-verticalelasticd.rb
@@ -16,6 +16,9 @@ QTD_VMS = 2
 # treshold maximo de cpu
 CPU_MAX  = 20
 
+# treshold minimo de cpu
+CPU_MIN  = 10
+
 # nome do servico. Usar o padrao "nonme-""
 VM_NOME = "mysql-"
 
@@ -216,7 +219,7 @@ while rodar == 1
         vm_nome_nova = create_new_vm(VM_NOME, TEMPLATE_O, client)
         while vm_status != 'RUNNING'
           vm_status  = get_status_vm(vm_nome_nova, client)
-          sleep(60)
+          sleep(10)
         end
       end
     end
@@ -243,14 +246,27 @@ while rodar == 1
       puts consumo_cpu.to_f
       puts vm_nome_antiga
       
-      if consumo_cpu.to_f > CPU_MAX.to_f 
-        
-        vm_nome_nova = create_new_vm(VM_NOME, TEMPLATE_N1, client)
+      if (consumo_cpu.to_f > CPU_MAX.to_f) and (!vm_nome_antiga.include? "n1-")
+        vm_nome_nova = create_new_vm(VM_NOME+"n1-", TEMPLATE_N1, client)
         vm_status = ''
         
         while vm_status != 'RUNNING'
           vm_status  = get_status_vm(vm_nome_nova, client)
-          sleep(60)
+          sleep(10)
+        end
+        # maquina removida quando a nova estiver ok.
+        remove_old_vm(vm_nome_antiga,client)
+      else
+        puts "Nao foi necessario elevar a VM."
+      end
+
+      if (consumo_cpu.to_f < CPU_MIN.to_f) and (vm_nome_antiga.include? "n1-")
+        vm_nome_nova = create_new_vm(VM_NOME, TEMPLATE_O, client)
+        vm_status = ''
+        
+        while vm_status != 'RUNNING'
+          vm_status  = get_status_vm(vm_nome_nova, client)
+          sleep(10)
         end
         # maquina removida quando a nova estiver ok.
         remove_old_vm(vm_nome_antiga,client)
@@ -260,153 +276,3 @@ while rodar == 1
 
 end
 
-#puts " VALOR FINAL == #{get_cpu_value_by_vm(vms_encontradas)}"
-
-
-
-
-# lista_vm_com_metrica = Array.new
-
-# vms_encontradas.each do |vm|
-#   if ((vm.lcm_state_str <=> 'RUNNING') == 0)
-#     vm_com_metrica = Array.new
-#     cpu_object = vm.monitoring(['MONITORING/CPU'])
-#     cpu_values = cpu_object.fetch('MONITORING/CPU')
-#     last_value_cpu = cpu_values[cpu_values.length() -1][1].to_f 
-#     puts cpu_object
-#     puts cpu_values[cpu_values.length() -1][1].to_f
-#     puts "+++++++"
-#     puts " "
-#     vm_com_metrica.push(vm.name)
-#     vm_com_metrica.push(last_value_cpu)
-#     lista_vm_com_metrica.push(vm_com_metrica)
-#   end
-# end 
-
-#puts lista_vm_com_metrica
-
-
-# vm_pool.each do |vm|
-#   vm.info
-#   r = Regexp.new(VM_NOME)
-#   #verificamos se ha vms do servico em questao, se nao tiver criar essa miseria
-#   if (r.match(vm.name.to_s).nil?)
-#     puts "passei no create, pois nao tinha vm para o servico"
-#     create_new_vm(VM_NOME, TEMPLATE_O, client)
-#   end
-#     # se a vm for encontrada e estiver rodando, coletamos o uso da cpu
-#   if ((vm.lcm_state_str <=> 'RUNNING') == 0)
-#     vm_filtrada = vm
-#     puts "passei no running"
-#   else
-#     puts "Nao existe maquina em RUNNING"
-#     exit -1
-#   end
-# end
-
-
-# # itera na lista de vms encontradas no Nebuloso
-
-
-# if ((vm_filtrada <=> '') != 0)
-#    metricas  = vm_filtrada.monitoring(['MONITORING/CPU'])
-#    metricas_cpu = metricas.fetch('MONITORING/CPU')
-#    cpu_metrica_valor_final = metricas_cpu[metricas_cpu.length() -1][1].to_f
-#    puts pu_metrica_valor_final
-#  else
-#   puts "Nao foram encontradas maquinas"
-#   exit -1
-# end
-
-
-# #metricas de cada vm
-# cpu_metrics_by_vm = Hash.new
-# cpu_values = Array.new
-# cpu_value_final # valor final da cpu
-
-# #verifico se foi encontrado alguma vm com o padrao do parametro
-# if (vm_filtradas.length = 1)
-#     #itera para pegar os dados de cada vm
-#     vms_filtradas.each do |vm_filtrada|  
-#       cpu_metrics_by_vm = vm_filtrada.monitoring(['MONITORING/CPU'])
-#       cpu_values = cpu_metrics_by_vm.fetch('MONITORING/CPU')
-      
-#       #valor do ultimo check 
-#       cpu_value_final = cpu_values[cpu_values.length() -1][1].to_f
-
-#       #verifica em qual check esta agora
-#       if(rodada >= QTD_CHECKS)
-
-#         #3) Verificar se estas máquinas ultrapassaram o limite de hardware (memoria ou cpu);       
-#         if(val_cpu_final > MET_CPU_MAX)       
-#           #4) Gerar uma nova máquina com 30%mais recurso de memória e/ou mais 1 cpu;
-#           template = <<-EOT
-#           CONTEXT = [
-#             NETWORK = "YES",
-#             REPORT_READY = "YES",
-#             SSH_PUBLIC_KEY = "$USER[SSH_PUBLIC_KEY]",
-#             TOKEN = "YES" ]
-#           CPU = "0.3"
-#           DESCRIPTION = "A small GNU/Linux system for testing"
-#           DISK = [
-#             IMAGE = "ttylinux",
-#             IMAGE_UNAME = "oneadmin" ]
-#           FEATURES = [
-#             ACPI = "no",
-#             APIC = "no" ]
-#           GRAPHICS = [
-#             LISTEN = "0.0.0.0",
-#             TYPE = "VNC" ]
-#           INPUTS_ORDER = ""
-#           MEMORY = "128"
-#           MEMORY_UNIT_COST = "MB"
-#           OS = [
-#             BOOT = "disk0" ]
-#           EOT
-
-#           # Creates a VirtualMachine description
-#           xml = OpenNebula::VirtualMachine.build_xml
-#           vm  = OpenNebula::VirtualMachine.new(xml, client)
-
-#           # VirtualMachine new name
-#           NEWNAME = VM_NOME
-
-#           # Creates a VirtualMachine and bring it up
-#           rc = vm.allocate(template)
-#           if OpenNebula.is_error?(rc)
-#               STDERR.puts rc.message
-#               exit(-1)
-#           else
-#               vm.rename(NEWNAME + vm.id.to_s)
-#               puts "New VM Started:\nID = #{vm.id.to_s} "
-#               vm.info
-#               puts vm.name
-#               puts vm.state
-
-
-#               #5) Excluir a máquina antiga;
-#               if vm_filtrada.name.to_s
-#                  #delete vm
-#                  rc = vm_filtrada.delete
-#                  if OpenNebula.is_error?(rc)
-#                       puts "Virtual Machine #{vm.id}: #{rc.message}"
-
-#                   else
-#                       puts "Virtual Machine #{vm.id}: Shutting down and Delete after!"
-
-#                   end
-#               end
-#           end
-#         end
-        
-#         if(val_cpu_final <= MET_CPU_MAX)       
-#           #4) Voltar o hardware antigo
-#            # ====COLOCAR O SCRIPT AQUI====#
-#            #5) Excluir a máquina antiga;
-#            # ====COLOCAR O SCRIPT AQUI====#
-#         end   
-#       end
-#     end
-# end
-# #tempo antes de repetir a consulta dos hosts
-# sleep(INTERVALO.minutes)
